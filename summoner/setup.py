93d8c61b35eeab453c547efcf46b7fc5efd58111
Remy Tuyeras <rtuyeras@gmail.com>
2025-05-04 09:47:28 -0400

    changed setup back

--- summoner/setup.py
+++ summoner/setup.py
@@ -6,8 +6,7 @@ setup(
     description="Summoner's SDK",
     author="Remy Tuyeras, Elliott Dehnbostel",
     author_email="rtuyeras@summoner.to",
-    packages=find_packages(),
-    # packages=find_packages(include=["summoner", "summoner.*"]),
+    packages=find_packages(include=["summoner", "summoner.*"]),
     install_requires=[
         "asyncio==3.4.3",
         "aioconsole==0.8.1",
