1c8b12e5a8ee673f03ec696114ff58cad19b746c
Remy Tuyeras <rtuyeras@gmail.com>
2025-05-13 09:16:46 -0400

    changes in python path insert

--- summoner/server/server.py
+++ summoner/server/server.py
@@ -6,10 +6,9 @@ import json
 from typing import Optional
 
 # Setup path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, ".."))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
+target_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+if target_path not in sys.path:
+    sys.path.insert(0, target_path)
 
 # Imports
 from utils import (
