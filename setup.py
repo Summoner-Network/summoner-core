4fe6a588fc5fb078ac6bbe7a2f9d3f64721d01a7
Remy Tuyeras <rtuyeras@gmail.com>
2026-04-30 06:18:08 -0400

    updating dotenv version back to 1.2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     python_requires=">=3.9",
     install_requires=[
         "aioconsole==0.8.1",
-        "python-dotenv~=1.2.1",
+        "python-dotenv~=1.2.2",
         "typing_extensions==4.15.0; python_version < '3.13'",
     ],
     extras_require={
